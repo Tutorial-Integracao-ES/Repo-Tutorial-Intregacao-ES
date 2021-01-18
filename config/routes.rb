d2ab093a661e5628813fa3995386732e2248bee5
David Manuel <davidmanuelpe@gmail.com>
2021-01-18 09:50:21 -0300

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'search', to: "articles#search"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles do
-    resources :comments
-  end
-  root 'welcome#index'
+  resources :admins
+  root 'admins#new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
