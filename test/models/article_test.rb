dc76b97671efdb5f67a6b436a21169966de6f155
David Manuel <davidmanuelpe@gmail.com>
2021-01-18 05:38:27 -0300

    subindo projeto

---
+++ test/models/article_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+
+  test 'should not save article without title' do
+    article = Article.new
+    assert_not article.save
+  end
+
+  test 'should save article with correct title' do
+    article = Article.new title: 'Ruby is awesome'
+    assert article.save
+  end
+end
